18f0ffe65540093a900669e658dd8ad537814caf
Robert Maksymow <114484269+RobertMaksymow@users.noreply.github.com>
2023-02-01 14:24:53 +0000

    Initial commit

---
+++ src/test/java/tech/outatime/todoappserver/TodoAppServerApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.outatime.todoappserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAppServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
